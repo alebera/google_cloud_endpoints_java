643f5381f20160ce0bd169b1009834393f4e138b
alessandroberardinelli <alessandro.berardinelli@sytac.io>
2025-06-26 23:21:46 +0200

    first commit

---
+++ src/main/java/com/example/echo/Post.java
@@ -0,0 +1,65 @@
+package com.example.echo;
+
+//import com.googlecode.objectify.annotation.Entity;
+//import com.googlecode.objectify.annotation.Id;
+
+import java.util.Date;
+
+//@Entity
+public class Post {
+//    @Id
+    private Long id;
+    private String author;
+    private String subject;
+    private String body;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ src/main/java/com/example/echo/PostEndpoints.java
@@ -0,0 +1,58 @@
+package com.example.echo;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.Named;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Api(
+        name = "postApi",
+        version = "v1",
+        description = "API to handle post"
+)
+public class PostEndpoints {
+
+//    private final PostService service = new PostService();
+
+    @ApiMethod(name = "createPost", path = "posts", httpMethod = ApiMethod.HttpMethod.POST)
+    public Post createPost(Post post) {
+        String currentUserEmail = "user@example.com";
+//        return service.createPost(post, currentUserEmail);
+        Post post1 = new Post();
+        post1.setAuthor("author1");
+        return post1;
+    }
+
+    @ApiMethod(name = "getPost", path = "posts/{id}", httpMethod = ApiMethod.HttpMethod.GET)
+    public Post getPost(@Named("id") Long id) {
+//        return service.getPost(id);
+        Post post1 = new Post();
+        post1.setAuthor("author1");
+        return post1;
+    }
+
+    @ApiMethod(name = "updatePost", path = "posts", httpMethod = ApiMethod.HttpMethod.PUT)
+    public Post updatePost(Post post) {
+        String currentUserEmail = "user@example.com";
+//        return service.updatePost(post, currentUserEmail);
+        Post post1 = new Post();
+        post1.setAuthor("author1");
+        return post1;
+    }
+
+    @ApiMethod(
+            name = "listPosts",
+            path = "posts",
+            httpMethod = ApiMethod.HttpMethod.GET
+    )
+    public List<Post> listPosts() {
+//        return service.listPosts();
+        Post post1 = new Post();
+        post1.setAuthor("author1");
+        return Arrays.asList(post1);
+    }
+}
+
